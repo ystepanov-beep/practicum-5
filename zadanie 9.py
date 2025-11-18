4068f90f263d7e92d785100723f3d31e6b8f05b2
ystepanov-beep <y.stepanov@g.nsu.ru>
2025-11-18 10:58:19 +0700

    Update zadanie 9.py

--- zadanie 9.py
+++ zadanie 9.py
@@ -1,3 +1,13 @@
 A, B, C = map(int,input().split())
-if A > B > C:
+if A >= B >= C:
     print(A, B, C)
+elif B >= C >= A:
+    print(B,C,A)
+elif C >= B >= A:
+    print(C, B, A)
+elif B >= A >= C:
+    print(B, A, C)
+elif A >= C >= B:
+    print(A, C, B)
+else:
+    print(C, A, B)
